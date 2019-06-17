d480958358943387c2d2d5a08f8a8bb8817f2fec
Victor Ronchetti <vronchetti@audioscience.com>
2019-06-16 20:02:58 -0700

    update iyo mod functions for hwioc revisio

--- ports/unix/iyo_hwio_mod.c
+++ ports/unix/iyo_hwio_mod.c
@@ -79,8 +79,8 @@ STATIC mp_obj_t mod_iyo_hwio_set_led_diagnostic(mp_obj_t enable, mp_obj_t rgb_pw
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_2(mod_iyo_hwio_set_led_diagnostic_obj, mod_iyo_hwio_set_led_diagnostic);
 
-STATIC mp_obj_t mod_iyo_hwio_subscribe_rx_ch_to_aes67_transmitter(mp_obj_t rx_ch_idx, mp_obj_t tx_flow_ip, mp_obj_t tx_flow_idx) {
-    int r = hwioc_subscribe_rx_ch_to_aes67_transmitter(mp_obj_get_int(rx_ch_idx), mp_obj_str_get_str(tx_flow_ip), mp_obj_get_int(tx_flow_idx));
+STATIC mp_obj_t mod_iyo_hwio_add_aes67_transmit_flow(mp_obj_t base_ch_idx, mp_obj_t channel_count) {
+    int r = hwioc_add_aes67_transmit_flow(mp_obj_get_int(base_ch_idx), mp_obj_get_int(channel_count));
 
     if (r < 0) {
         mp_raise_OSError(-r);
@@ -88,18 +88,7 @@ STATIC mp_obj_t mod_iyo_hwio_subscribe_rx_ch_to_aes67_transmitter(mp_obj_t rx_ch
 
     return mp_const_none;
 }
-STATIC MP_DEFINE_CONST_FUN_OBJ_3(mod_iyo_hwio_subscribe_rx_ch_to_aes67_transmitter_obj, mod_iyo_hwio_subscribe_rx_ch_to_aes67_transmitter);
-
-STATIC mp_obj_t mod_iyo_hwio_add_aes67_transmit_flow(mp_obj_t session_name, mp_obj_t base_ch_idx, mp_obj_t channel_count) {
-    int r = hwioc_add_aes67_transmit_flow(mp_obj_str_get_str(session_name), mp_obj_get_int(base_ch_idx), mp_obj_get_int(channel_count));
-
-    if (r < 0) {
-        mp_raise_OSError(-r);
-    }
-
-    return mp_const_none;
-}
-STATIC MP_DEFINE_CONST_FUN_OBJ_3(mod_iyo_hwio_add_aes67_transmit_flow_obj, mod_iyo_hwio_add_aes67_transmit_flow);
+STATIC MP_DEFINE_CONST_FUN_OBJ_2(mod_iyo_hwio_add_aes67_transmit_flow_obj, mod_iyo_hwio_add_aes67_transmit_flow);
 
 STATIC mp_obj_t mod_iyo_hwio_delete_aes67_transmit_flow(mp_obj_t base_ch_idx, mp_obj_t channel_count) {
     int r = hwioc_delete_aes67_transmit_flow(mp_obj_get_int(base_ch_idx), mp_obj_get_int(channel_count));
@@ -112,6 +101,28 @@ STATIC mp_obj_t mod_iyo_hwio_delete_aes67_transmit_flow(mp_obj_t base_ch_idx, mp
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_2(mod_iyo_hwio_delete_aes67_transmit_flow_obj, mod_iyo_hwio_delete_aes67_transmit_flow);
 
+STATIC mp_obj_t mod_iyo_hwio_add_aes67_receive_flow(mp_obj_t rx_sub_ip, mp_obj_t channels) {
+    int r = hwioc_add_aes67_receive_flow(mp_obj_str_get_str(rx_sub_ip), mp_obj_str_get_str(channels));
+
+    if (r < 0) {
+        mp_raise_OSError(-r);
+    }
+
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_2(mod_iyo_hwio_add_aes67_receive_flow_obj, mod_iyo_hwio_add_aes67_receive_flow);
+
+STATIC mp_obj_t mod_iyo_hwio_delete_aes67_receive_flow(mp_obj_t rx_sub_ip) {
+    int r = hwioc_delete_aes67_receive_flow(mp_obj_str_get_str(rx_sub_ip));
+
+    if (r < 0) {
+        mp_raise_OSError(-r);
+    }
+
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(mod_iyo_hwio_delete_aes67_receive_flow_obj, mod_iyo_hwio_delete_aes67_receive_flow);
+
 STATIC mp_obj_t mod_iyo_hwio_set_save_pending_flag(mp_obj_t pending) {
     int r = hwioc_set_save_pending_flag(!!mp_obj_get_int(pending));
 
@@ -122,16 +133,6 @@ STATIC mp_obj_t mod_iyo_hwio_set_save_pending_flag(mp_obj_t pending) {
     return mp_const_none;
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(mod_iyo_hwio_set_save_pending_flag_obj, mod_iyo_hwio_set_save_pending_flag);
-
-STATIC mp_obj_t mod_iyo_hwio_get_dante_device_name(void) {
-    char * name = hwioc_get_dante_device_name();
-    if (!name) {
-        mp_raise_OSError(-1);
-    }
-
-    return MP_OBJ_FROM_PTR(MP_OBJ_NEW_QSTR(qstr_from_str(name)));
-}
-STATIC MP_DEFINE_CONST_FUN_OBJ_0(mod_iyo_hwio_get_dante_device_name_obj, mod_iyo_hwio_get_dante_device_name);
 
 STATIC mp_obj_t mod_iyo_hwio_send_reboot_cmd(void) {
     int r = hwioc_send_device_reboot_cmd();
@@ -155,17 +156,6 @@ STATIC mp_obj_t mod_iyo_hwio_signal_objects_restore_done(void) {
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_0(mod_iyo_hwio_signal_objects_restore_done_obj, mod_iyo_hwio_signal_objects_restore_done);
 
-STATIC mp_obj_t mod_iyo_hwio_get_aes67_enabled(void) {
-    int r = hwioc_get_aes67_enabled();
-
-    if (r < 0) {
-        mp_raise_OSError(-r);
-    }
-
-    return MP_OBJ_FROM_PTR(mp_obj_new_int(r));
-}
-STATIC MP_DEFINE_CONST_FUN_OBJ_0(mod_iyo_hwio_get_aes67_enabled_obj, mod_iyo_hwio_get_aes67_enabled);
-
 STATIC mp_obj_t mod_iyo_hwio_send_identify_cmd(mp_obj_t enable) {
     int r = hwioc_send_device_identify_cmd(!!mp_obj_get_int(enable));
 
@@ -179,13 +169,19 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_1(mod_iyo_hwio_send_identify_cmd_obj, mod_iyo_hwi
 
 STATIC mp_obj_t mod_iyo_hwio_get_fds(void) {
     int m_fd = hwioc_meter_update_fd();
-    int e_fd = hwioc_error_notification_fd();
-    int a_fd = hwioc_aes67_tx_discovery_fd();
+    int c_fd = hwioc_conmon_to_py_fd();
+    int a_fd = hwioc_conmon_aes67_to_py_fd();
+    int cs_fd = hwioc_conmon_str_to_py_fd();
+    int x_fd = hwioc_xmos_to_py_fd();
+    int xs_fd = hwioc_xmos_str_to_py_fd();
 
-    mp_obj_tuple_t *t = MP_OBJ_TO_PTR(mp_obj_new_tuple(3, NULL));
+    mp_obj_tuple_t *t = MP_OBJ_TO_PTR(mp_obj_new_tuple(6, NULL));
     t->items[0] = mp_obj_new_int(m_fd);
-    t->items[1] = mp_obj_new_int(e_fd);
+    t->items[1] = mp_obj_new_int(c_fd);
     t->items[2] = mp_obj_new_int(a_fd);
+    t->items[3] = mp_obj_new_int(cs_fd);
+    t->items[4] = mp_obj_new_int(x_fd);
+    t->items[5] = mp_obj_new_int(xs_fd);
 
     return MP_OBJ_FROM_PTR(t);
 }
@@ -200,95 +196,6 @@ STATIC mp_obj_t mod_iyo_hwio_set_front_panel_brightness(mp_obj_t brightness) {
     return mp_const_none;
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(mod_iyo_hwio_set_front_panel_brightness_obj, mod_iyo_hwio_set_front_panel_brightness);
-
-STATIC mp_obj_t mod_iyo_hwio_get_dev_status(void) {
-    char * dev_sync_status = hwioc_get_device_sync_status();
-    char * dev_sys_status = hwioc_get_device_sys_status();
-
-    mp_obj_tuple_t *t = MP_OBJ_TO_PTR(mp_obj_new_tuple(2, NULL));
-    t->items[0] = MP_OBJ_NEW_QSTR(qstr_from_str(dev_sync_status));
-    t->items[1] = MP_OBJ_NEW_QSTR(qstr_from_str(dev_sys_status));
-
-    return MP_OBJ_FROM_PTR(t);
-}
-STATIC MP_DEFINE_CONST_FUN_OBJ_0(mod_iyo_hwio_get_dev_status_obj, mod_iyo_hwio_get_dev_status);
-
-STATIC mp_obj_t mod_iyo_hwio_get_dev_temp(void) {
-    int dev_temp = hwioc_get_device_temp();
-
-    return MP_OBJ_FROM_PTR(mp_obj_new_int(dev_temp));
-}
-STATIC MP_DEFINE_CONST_FUN_OBJ_0(mod_iyo_hwio_get_dev_temp_obj, mod_iyo_hwio_get_dev_temp);
-
-STATIC mp_obj_t mod_iyo_hwio_get_mac_address(void) {
-    uint8_t * mac = hwioc_get_mac_address();
-    if (!mac) {
-        mp_raise_OSError(-1);
-    }
-
-    return MP_OBJ_FROM_PTR(mp_obj_new_bytearray(6, mac));
-}
-STATIC MP_DEFINE_CONST_FUN_OBJ_0(mod_iyo_hwio_get_mac_address_obj, mod_iyo_hwio_get_mac_address);
-
-STATIC mp_obj_t mod_iyo_hwio_get_ip_address(void) {
-    uint8_t * ip = hwioc_get_ip_address_bytes();
-    if (!ip) {
-        mp_raise_OSError(-1);
-    }
-
-    return MP_OBJ_FROM_PTR(mp_obj_new_bytearray(4, ip));
-}
-STATIC MP_DEFINE_CONST_FUN_OBJ_0(mod_iyo_hwio_get_ip_address_obj, mod_iyo_hwio_get_ip_address);
-
-STATIC mp_obj_t mod_iyo_hwio_get_tx_ch_aes67_transport_ip(mp_obj_t index) {
-    uint8_t * ip = hwioc_get_tx_ch_aes67_transport_ip(mp_obj_get_int(index));
-    if (!ip) {
-        mp_raise_OSError(-1);
-    }
-
-    return MP_OBJ_FROM_PTR(mp_obj_new_bytearray(4, ip));
-}
-STATIC MP_DEFINE_CONST_FUN_OBJ_1(mod_iyo_hwio_get_tx_ch_aes67_transport_ip_obj, mod_iyo_hwio_get_tx_ch_aes67_transport_ip);
-
-STATIC mp_obj_t mod_iyo_hwio_get_tx_ch_aes67_flow_channel(mp_obj_t index) {
-    int flow_channel = hwioc_get_tx_ch_aes67_flow_channel(mp_obj_get_int(index));
-    if (flow_channel < 0) {
-        mp_raise_OSError(-1);
-    }
-
-    return MP_OBJ_FROM_PTR(mp_obj_new_int(flow_channel));
-}
-STATIC MP_DEFINE_CONST_FUN_OBJ_1(mod_iyo_hwio_get_tx_ch_aes67_flow_channel_obj, mod_iyo_hwio_get_tx_ch_aes67_flow_channel);
-
-STATIC mp_obj_t mod_iyo_hwio_get_rx_ch_aes67_sub_transport_ip(mp_obj_t index) {
-    uint8_t * ip = hwioc_get_rx_ch_aes67_sub_transport_ip(mp_obj_get_int(index));
-    if (!ip) {
-        mp_raise_OSError(-1);
-    }
-
-    return MP_OBJ_FROM_PTR(mp_obj_new_bytearray(4, ip));
-}
-STATIC MP_DEFINE_CONST_FUN_OBJ_1(mod_iyo_hwio_get_rx_ch_aes67_sub_transport_ip_obj, mod_iyo_hwio_get_rx_ch_aes67_sub_transport_ip);
-
-STATIC mp_obj_t mod_iyo_hwio_get_rx_ch_aes67_sub_channel_index(mp_obj_t index) {
-    int ch_idx = hwioc_get_rx_ch_aes67_sub_channel_index(mp_obj_get_int(index));
-    if (ch_idx < 0) {
-        mp_raise_OSError(-1);
-    }
-
-    return MP_OBJ_FROM_PTR(mp_obj_new_int(ch_idx));
-}
-STATIC MP_DEFINE_CONST_FUN_OBJ_1(mod_iyo_hwio_get_rx_ch_aes67_sub_channel_index_obj, mod_iyo_hwio_get_rx_ch_aes67_sub_channel_index);
-
-STATIC mp_obj_t mod_iyo_hwio_get_rx_ch_aes67_sub_connections_active(mp_obj_t index) {
-    int connections_active = hwioc_get_rx_ch_aes67_sub_connections_active(mp_obj_get_int(index));
-    if (connections_active < 0) {
-        mp_raise_OSError(-1);
-    }
-
-    return MP_OBJ_FROM_PTR(mp_obj_new_int(connections_active));
-}
-STATIC MP_DEFINE_CONST_FUN_OBJ_1(mod_iyo_hwio_get_rx_ch_aes67_sub_connections_active_obj, mod_iyo_hwio_get_rx_ch_aes67_sub_connections_active);
 
 STATIC const mp_rom_map_elem_t mp_module_iyo_hwio_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_iyo_hwio) },
@@ -310,37 +217,17 @@ STATIC const mp_rom_map_elem_t mp_module_iyo_hwio_globals_table[] = {
 
     { MP_ROM_QSTR(MP_QSTR_signal_objects_restore_done), MP_ROM_PTR(&mod_iyo_hwio_signal_objects_restore_done_obj) },
 
-    { MP_ROM_QSTR(MP_QSTR_get_aes67_enabled), MP_ROM_PTR(&mod_iyo_hwio_get_aes67_enabled_obj) },
-
     { MP_ROM_QSTR(MP_QSTR_send_identify_cmd), MP_ROM_PTR(&mod_iyo_hwio_send_identify_cmd_obj) },
 
     { MP_ROM_QSTR(MP_QSTR_get_fds), MP_ROM_PTR(&mod_iyo_hwio_get_fds_obj) },
 
-    { MP_ROM_QSTR(MP_QSTR_get_dev_status), MP_ROM_PTR(&mod_iyo_hwio_get_dev_status_obj) },
-
-    { MP_ROM_QSTR(MP_QSTR_get_dev_temp), MP_ROM_PTR(&mod_iyo_hwio_get_dev_temp_obj) },
-
-    { MP_ROM_QSTR(MP_QSTR_get_mac_address), MP_ROM_PTR(&mod_iyo_hwio_get_mac_address_obj) },
-
-    { MP_ROM_QSTR(MP_QSTR_get_ip_address), MP_ROM_PTR(&mod_iyo_hwio_get_ip_address_obj) },
-
-    { MP_ROM_QSTR(MP_QSTR_get_dante_device_name), MP_ROM_PTR(&mod_iyo_hwio_get_dante_device_name_obj) },
-
-    { MP_ROM_QSTR(MP_QSTR_get_tx_ch_aes67_transport_ip), MP_ROM_PTR(&mod_iyo_hwio_get_tx_ch_aes67_transport_ip_obj) },
-
-    { MP_ROM_QSTR(MP_QSTR_get_tx_ch_aes67_flow_channel), MP_ROM_PTR(&mod_iyo_hwio_get_tx_ch_aes67_flow_channel_obj) },
-
-    { MP_ROM_QSTR(MP_QSTR_get_rx_ch_aes67_sub_transport_ip), MP_ROM_PTR(&mod_iyo_hwio_get_rx_ch_aes67_sub_transport_ip_obj) },
-
-    { MP_ROM_QSTR(MP_QSTR_get_rx_ch_aes67_sub_channel_index), MP_ROM_PTR(&mod_iyo_hwio_get_rx_ch_aes67_sub_channel_index_obj) },
-
-    { MP_ROM_QSTR(MP_QSTR_get_rx_ch_aes67_sub_connections_active), MP_ROM_PTR(&mod_iyo_hwio_get_rx_ch_aes67_sub_connections_active_obj) },
-
-    { MP_ROM_QSTR(MP_QSTR_subscribe_rx_ch_to_aes67_transmitter), MP_ROM_PTR(&mod_iyo_hwio_subscribe_rx_ch_to_aes67_transmitter_obj) },
-
     { MP_ROM_QSTR(MP_QSTR_add_aes67_transmit_flow), MP_ROM_PTR(&mod_iyo_hwio_add_aes67_transmit_flow_obj) },
 
     { MP_ROM_QSTR(MP_QSTR_delete_aes67_transmit_flow), MP_ROM_PTR(&mod_iyo_hwio_delete_aes67_transmit_flow_obj) },
+
+    { MP_ROM_QSTR(MP_QSTR_add_aes67_receive_flow), MP_ROM_PTR(&mod_iyo_hwio_add_aes67_receive_flow_obj) },
+
+    { MP_ROM_QSTR(MP_QSTR_delete_aes67_receive_flow), MP_ROM_PTR(&mod_iyo_hwio_delete_aes67_receive_flow_obj) },
 
     { MP_ROM_QSTR(MP_QSTR_set_front_panel_brightness), MP_ROM_PTR(&mod_iyo_hwio_set_front_panel_brightness_obj) },
 };
