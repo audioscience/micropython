9a085234ef074abfff83769242cc6006ee36edb0
Delio Brignoli <dbrignoli@audioscience.com>
2021-02-27 10:48:40 +0000

    unix: enable MICROPY_PY_OS_DUPTERM and set it to 3.

--- ports/unix/mpconfigport.h
+++ ports/unix/mpconfigport.h
@@ -137,6 +137,7 @@
 #define MICROPY_STACKLESS_STRICT    (0)
 #endif
 
+#define MICROPY_PY_OS_DUPTERM       (3)
 #define MICROPY_PY_OS_STATVFS       (1)
 #define MICROPY_PY_UTIME            (1)
 #define MICROPY_PY_UTIME_MP_HAL     (1)
