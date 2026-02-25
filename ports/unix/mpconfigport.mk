36cc9c39f263158dd4d52f4c4a17e01b3f8eeb02
Delio Brignoli <dbrignoli@audioscience.com>
2026-02-25 11:43:23 +0100

    ports:unix:mpconfigport.mk: enable standalone build

--- ports/unix/mpconfigport.mk
+++ ports/unix/mpconfigport.mk
@@ -38,7 +38,7 @@ MICROPY_PY_JNI = 0
 
 # Avoid using system libraries, use copies bundled with MicroPython
 # as submodules (currently affects only libffi).
-MICROPY_STANDALONE ?= 0
+MICROPY_STANDALONE ?= 1
 
 MICROPY_ROM_TEXT_COMPRESSION = 1
 
